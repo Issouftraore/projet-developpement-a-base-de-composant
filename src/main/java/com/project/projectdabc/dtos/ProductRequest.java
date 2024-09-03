a0e4c4652274154e72772698065ae031eecf8e17
Traore Issouf <i.traore@agitex.fr>
2024-09-03 20:57:55 +0000

    finalisation du powerpoint

--- src/main/java/com/project/projectdabc/dtos/ProductRequest.java
+++ src/main/java/com/project/projectdabc/dtos/ProductRequest.java
@@ -6,7 +6,9 @@ import java.util.List;
 @Data
 public class ProductRequest {
     private String name;
+
     private String description;
+
     private Double price;
     private List<Long> categoryIds;
 }
